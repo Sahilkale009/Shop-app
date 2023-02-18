f0c4686346e921af063d7150cb63687a700af89f
Sahil Kale <sahil.kale20@pccoepune.org>
2023-02-18 12:51:34 +0530

    Chreate Shop App

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  @override
+  Widget build(BuildContext context) {
+    //final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(builder: (context, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+            } else {
+              return Consumer<Orders>(
+                builder: (context, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (context, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        }));
+  }
+}
